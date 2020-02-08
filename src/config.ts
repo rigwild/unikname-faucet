1cb6c537cedbbf04f94a028eed992fff72d38fcd
rigwild <me@rigwild.dev>
2020-02-08 20:45:29 +0100

    Add favicon + fix config parse float

--- src/config.ts
+++ src/config.ts
@@ -12,8 +12,8 @@ const env = process.env as { [key: string]: string }
 export const { NETWORK, PASSPHRASE, SECOND_PASSPHRASE, GIFT_VENDORFIELD } = env
 export const SERVER_PORT = parseInt(env.SERVER_PORT, 10)
 export const GIFT_INTERVAL_DELAY_MS = parseInt(env.GIFT_INTERVAL_DELAY_MS, 10)
-export const GIFT_AMOUNT = parseInt(env.GIFT_AMOUNT, 10)
-export const GIFT_FEE = parseInt(env.GIFT_FEE, 10)
+export const GIFT_AMOUNT = parseFloat(env.GIFT_AMOUNT)
+export const GIFT_FEE = parseFloat(env.GIFT_FEE)
 
 export const isWindows = os.platform() === 'win32'
 export const unsExecutable = path.resolve(__dirname, '..', 'node_modules', '.bin', isWindows ? 'uns.cmd' : 'uns')
